6313ee2e64e3a3cfcf39a657e088a4ddeb24230a
Georges Auberger <georges@ternarylabs.com>
2015-01-28 13:24:35 -0800

    Updated demo files

--- example/sandbox.ternarylabs.com/porthole/js/porthole.js
+++ example/sandbox.ternarylabs.com/porthole/js/porthole.js
@@ -44,62 +44,63 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
  */
 // Inspired by base2 and Prototype
 (function(){
-  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-  // The base Class implementation (does nothing)
-  this.Class = function(){};
-  
-  // Create a new Class that inherits from this class
-  Class.extend = function(prop) {
-    var _super = this.prototype;
-    
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    initializing = true;
-    var prototype = new this();
-    initializing = false;
-    
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = typeof prop[name] == "function" && 
-        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-        (function(name, fn){
-          return function() {
-            var tmp = this._super;
-            
-            // Add a new ._super() method that is the same method
-            // but on the super-class
-            this._super = _super[name];
-            
-            // The method only need to be bound temporarily, so we
-            // remove it when we're done executing
-            var ret = fn.apply(this, arguments);        
-            this._super = tmp;
-            
-            return ret;
-          };
-        })(name, prop[name]) :
-        prop[name];
-    }
-    
-    // The dummy class constructor
-    function Class() {
-      // All construction is actually done in the init method
-      if ( !initializing && this.init )
-        this.init.apply(this, arguments);
-    }
-    
-    // Populate our constructed prototype object
-    Class.prototype = prototype;
-    
-    // Enforce the constructor to be what we expect
-    Class.prototype.constructor = Class;
+    var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
 
-    // And make this class extendable
-    Class.extend = arguments.callee;
-    
-    return Class;
-  };
+    // The base Class implementation (does nothing)
+    this.PortholeClass = function(){};
+
+    // Create a new Class that inherits from this class
+    PortholeClass.extend = function(prop) {
+        var _super = this.prototype;
+
+        // Instantiate a base class (but only create the instance,
+        // don't run the init constructor)
+        initializing = true;
+        var prototype = new this();
+        initializing = false;
+
+        // Copy the properties over onto the new prototype
+        for (var name in prop) {
+            // Check if we're overwriting an existing function
+            prototype[name] = typeof prop[name] == "function" &&
+                typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+                (function(name, fn){
+                    return function() {
+                        var tmp = this._super;
+
+                        // Add a new ._super() method that is the same method
+                        // but on the super-class
+                        this._super = _super[name];
+
+                        // The method only need to be bound temporarily, so we
+                        // remove it when we're done executing
+                        var ret = fn.apply(this, arguments);
+                        this._super = tmp;
+
+                        return ret;
+                    };
+                })(name, prop[name]) :
+                prop[name];
+        }
+
+        // The dummy class constructor
+        function Class() {
+            // All construction is actually done in the init method
+            if ( !initializing && this.init )
+                this.init.apply(this, arguments);
+        }
+
+        // Populate our constructed prototype object
+        Class.prototype = prototype;
+
+        // Enforce the constructor to be what we expect
+        Class.prototype.constructor = Class;
+
+        // And make this class extendable
+        Class.extend = arguments.callee;
+
+        return Class;
+    };
 })();
 
 (function (window) {
@@ -114,12 +115,14 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
      * @module Porthole
      */
     var Porthole = {
+        debug: false,
+
         /**
          * Utility function to trace to console
          * @private
          */
         trace: function(s) {
-            if (window['console'] !== undefined) {
+            if (this.debug && window.console !== undefined) {
                 window.console.log('Porthole: ' + s);
             }
         },
@@ -129,7 +132,7 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
          * @private
          */
         error: function(s) {
-            if (window['console'] !== undefined) {
+            if (window.console !== undefined) {
                 window.console.error('Porthole: ' + s);
             }
         }
@@ -138,7 +141,7 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
     /**
      * @class
      * @classdesc Proxy window object to post message to target window
-     * @param {string} proxyIFrameUrl - Fully qualified url to proxy iframe
+     * @param {string} proxyIFrameUrl - Fully qualified url to proxy iframe, or null to create a receiver only window
      * @param {string} targetWindowName - Name of the proxy iframe window
      */
     Porthole.WindowProxy = function(){};
@@ -167,7 +170,7 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
         removeEventListener: function(f) {}
     };
 
-    Porthole.WindowProxyBase = Class.extend({
+    Porthole.WindowProxyBase = PortholeClass.extend({
         init: function(targetWindowName) {
             if (targetWindowName === undefined) {
                 targetWindowName = '';
@@ -244,7 +247,7 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
     Porthole.WindowProxyLegacy = Porthole.WindowProxyBase.extend({
         init: function(proxyIFrameUrl, targetWindowName) {
             this._super(targetWindowName);
-            
+
             if (proxyIFrameUrl !== null) {
                 this.proxyIFrameName = this.targetWindowName + 'ProxyIFrame';
                 this.proxyIFrameLocation = proxyIFrameUrl;
@@ -254,7 +257,8 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
             } else {
                 // Won't be able to send messages
                 this.proxyIFrameElement = null;
-                throw  new Error("proxyIFrameUrl can't be null");
+                Porthole.trace("proxyIFrameUrl is null, window will be a receiver only");
+                this.post = function(){ throw new Error("Receiver only window");};
             }
         },
 
@@ -312,8 +316,14 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
         addEventListener: function(f) {
             if (this.eventListeners.length === 0) {
                 var self = this;
-                this.eventListenerCallback = function(event) { self.eventListener(self, event); };
-                window.addEventListener('message', this.eventListenerCallback, false);
+                if (window.addEventListener) {
+                    this.eventListenerCallback = function(event) { self.eventListener(self, event); };
+                    window.addEventListener('message', this.eventListenerCallback, false);
+                } else if (window.attachEvent) {
+                    // Make IE8 happy, just not that 1. postMessage only works for IFRAMES/FRAMES http://blogs.msdn.com/b/ieinternals/archive/2009/09/16/bugs-in-ie8-support-for-html5-postmessage-sessionstorage-and-localstorage.aspx
+                    this.eventListenerCallback = function(event) { self.eventListener(self, window.event); };
+                    window.attachEvent("onmessage", this.eventListenerCallback);
+                }
             }
             return this._super(f);
         },
@@ -322,20 +332,26 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
             this._super(f);
 
             if (this.eventListeners.length === 0) {
-                window.removeEventListener('message', this.eventListenerCallback);
+                if (window.removeEventListener) {
+                    window.removeEventListener('message', this.eventListenerCallback);
+                } else if (window.detachEvent) { // Make IE8, happy, see above
+                    // see jquery, detachEvent needed property on element, by name of that event, to properly expose it to GC
+                    if (typeof window.onmessage === 'undefined') window.onmessage = null;
+                    window.detachEvent('onmessage', this.eventListenerCallback);
+                }
                 this.eventListenerCallback = null;
             }
         },
 
         eventListener: function(self, nativeEvent) {
             var data = Porthole.WindowProxy.unserialize(nativeEvent.data);
-            if (data && (self.targetWindowName == '' || data.sourceWindowName == self.targetWindowName)) {
+            if (data && (self.targetWindowName === '' || data.sourceWindowName == self.targetWindowName)) {
                 self.dispatchEvent(new Porthole.MessageEvent(data.data, nativeEvent.origin, self));
             }
         }
     });
 
-    if (typeof window.postMessage !== 'function') {
+    if (!window.postMessage) {
         Porthole.trace('Using legacy browser support');
         Porthole.WindowProxy = Porthole.WindowProxyLegacy.extend({});
     } else {
@@ -377,11 +393,11 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
 
     Porthole.WindowProxy.getTargetWindow = function(targetWindowName) {
         if (targetWindowName === '') {
-            return top;
+            return parent;
         } else if (targetWindowName === 'top' || targetWindowName === 'parent') {
             return window[targetWindowName];
-        } 
-        return parent.frames[targetWindowName];
+        }
+        return window.frames[targetWindowName];
     };
 
     /**
@@ -428,12 +444,12 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
                 if (windowProxy) {
                     if (windowProxy.origin === message.targetOrigin || message.targetOrigin === '*') {
                         windowProxy.dispatchEvent(
-                          new Porthole.MessageEvent(message.data, message.sourceOrigin, windowProxy));
+                            new Porthole.MessageEvent(message.data, message.sourceOrigin, windowProxy));
                     } else {
                         Porthole.error('Target origin ' +
-                                       windowProxy.origin +
-                                       ' does not match desired target of ' +
-                                       message.targetOrigin);
+                            windowProxy.origin +
+                            ' does not match desired target of ' +
+                            message.targetOrigin);
                     }
                 } else {
                     Porthole.error('Could not find window proxy object on the target window');
@@ -448,13 +464,9 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
         findWindowProxyObjectInWindow: function(w, sourceWindowName) {
             var i;
 
-            // IE does not enumerate global objects on the window object
-            if (w.RuntimeObject) {
-                w = w.RuntimeObject();
-            }
             if (w) {
                 for (i in w) {
-                    if (w.hasOwnProperty(i)) {
+                    if (Object.prototype.hasOwnProperty.call(w, i)) {
                         try {
                             // Ensure that we're finding the proxy object
                             // that is declared to be targetting the window that is calling us
@@ -480,10 +492,14 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
         start: function() {
             if (window.addEventListener) {
                 window.addEventListener('resize',
-                                        Porthole.WindowProxyDispatcher.forwardMessageEvent,
-                                        false);
+                    Porthole.WindowProxyDispatcher.forwardMessageEvent,
+                    false);
+            } else if (window.attachEvent && window.postMessage !== 'undefined') {
+                window.attachEvent('onresize',
+                    Porthole.WindowProxyDispatcher.forwardMessageEvent);
             } else if (document.body.attachEvent) {
-                window.attachEvent('onresize', Porthole.WindowProxyDispatcher.forwardMessageEvent);
+                window.attachEvent('onresize',
+                    Porthole.WindowProxyDispatcher.forwardMessageEvent);
             } else {
                 // Should never happen
                 Porthole.error('Cannot attach resize event');
